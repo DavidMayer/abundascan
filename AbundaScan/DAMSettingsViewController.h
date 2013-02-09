2b47a4979da854592fdaf73e6ffaccc0f69374ac
David Mayer <David.Allen.Mayer@gmail.com>
2013-02-09 15:28:39 -0800

    main view buttons with logic added

---
+++ AbundaScan/DAMSettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  DAMSettingsViewController.h
+//  AbundaScan
+//
+//  Created by David Mayer on 2/9/13.
+//  Copyright (c) 2013 David Mayer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DAMSettingsViewController : UIViewController
+
+@end
---
+++ AbundaScan/DAMSettingsViewController.m
@@ -0,0 +1,38 @@
+//
+//  DAMSettingsViewController.m
+//  AbundaScan
+//
+//  Created by David Mayer on 2/9/13.
+//  Copyright (c) 2013 David Mayer. All rights reserved.
+//
+
+#import "DAMSettingsViewController.h"
+
+@interface DAMSettingsViewController ()
+
+@end
+
+@implementation DAMSettingsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
